e561c82892453e8cbceb8ae48372348f6a214f44
Sameera Simha J <sameerasimhajayasimhabsc24@rvu.edu.in>
2026-03-26 12:25:03 +0530

    Update personal information in intro.py

--- intro.py
+++ intro.py
@@ -1,6 +1,6 @@
 print("Hello")
-print("My name is Sam")
-print("I am from class Bsc CS 2nd year")
+print("My name is Sameera")
+print("I am from class Bsc CS 3rd year")
 print("Section A")
 print("USN - 1RUA24SCS0099")
 
