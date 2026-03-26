b1824a7c7c5eb1f9355b82cb150a8c2c5cfc0692
7beastgamer <sameerasimhajayasimhabsc24@rvu.edu.in>
2026-03-26 12:10:34 +0530

    Intial Commit

---
+++ program2.py
@@ -0,0 +1,11 @@
+def camelcase(s):
+    count = 1 
+
+    for char in s:
+        if char.isupper():
+            count += 1
+
+    return count
+
+s = input("Enter camelCase string: ")
+print("Number of words:", camelcase(s))
